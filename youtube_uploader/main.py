b307b8cf4849130af4047659a1bf9bf3b1954a3a
Daniel Jung <idjung4@gmail.com>
2022-12-27 20:13:51 -0500

    Added adding to playlist after upload. Used native google api library

--- youtube_uploader/main.py
+++ youtube_uploader/main.py
@@ -1,128 +1,48 @@
 #!/usr/bin/python
-
 import os
-import json
-import googleapiclient
-from googleapiclient.errors import HttpError
-from youtube_upload.client import YoutubeUploader
-import time
 import os.path
-from google.oauth2.credentials import Credentials
+import time
+from datetime import datetime
 
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
-from google_auth_oauthlib.flow import InstalledAppFlow
+from youtube_uploader.utils.youtube import (
+    upload_video,
+    get_playlist_id_from_date,
+    insert_video_into_playlist,
+    get_authenticated_service,
+)
+
 CLIENT_SECRET_FILE = "client_secret_252723081857-nr72rralklriqt6mh3d1d5idm1kbh8a9.apps.googleusercontent.com.json"
-
-# client_secret_252723081857-nr72rralklriqt6mh3d1d5idm1kbh8a9.apps.googleusercontent.com.json
-# client_secret_551581634693-mrsqtg54795liv2e5djvaph46c10fbvu.apps.googleusercontent.com.json
-
-
-# This OAuth 2.0 access scope allows an application to upload files to the
-# authenticated user's YouTube channel, but doesn't allow other types of access.
-YOUTUBE_UPLOAD_SCOPES = [
-    "https://www.googleapis.com/auth/youtube.upload",
-    "https://www.googleapis.com/auth/youtubepartner-channel-audit",
-    "https://www.googleapis.com/auth/youtubepartner"
-]
-YOUTUBE_API_SERVICE_NAME = "youtube"
-YOUTUBE_API_VERSION = "v3"
-MISSING_CLIENT_SECRETS_MESSAGE = ""
 
 UPLOAD_DIRECTORY = r"C:\Users\Ilgoo\Videos\Bouldering"
 PHOTO_DATETIME_FORMAT = "%Y%m%d_%H%M%S"
 
 
-def get_user_credentials():
-    if os.path.isfile("credentials.json"):
-        return Credentials.from_authorized_user_file("credentials.json")
-    flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRET_FILE, YOUTUBE_UPLOAD_SCOPES)
-    credentials = flow.run_local_server()
-    with open("credentials.json", "w") as file:
-        test = json.loads(credentials.to_json())
-        json.dump(json.loads(credentials.to_json()), file)
-    return credentials
-
-
-def get_authenticated_service():
-  credentials = get_user_credentials()
-  return build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, credentials = credentials)
-
-
-def upload_video(uploader, file_name):
-    try:
-        options = {
-            "title": file_name[:-4].replace("_", " "),
-            "description": "",
-            "categoryId": "17",
-            "privacyStatus": "public",  # Video privacy. Can either be "public", "private", or "unlisted"
-            "kids": False,  # Specifies if the Video if for kids or not. Defaults to False.
-            # Optional. Specifies video thumbnail.
-        }
-        file_path = f"{UPLOAD_DIRECTORY}/{file_name}"
-        upload_response = uploader.upload(file_path, options)
-        print(upload_response)
-
-        if "id" in upload_response[0]:
-            print(f"Deleting {file_path}")
-            os.remove(file_path)
-        return upload_response
-    except googleapiclient.errors.ResumableUploadError as e:
-        print(e.reason)
-        exit()
-
-
-def main():
-    service = get_authenticated_service()
-
-    playlists = service.playlists().list(channelId="UCO8DUUQN0rVnvsjmrNiOjrg", part="snippet,contentDetails").execute()
-    print(playlists)
-
-
-def main2():
-    uploader = YoutubeUploader(secrets_file_path=CLIENT_SECRET_FILE)
-
-    uploader.authenticate()
-
-    youtube = uploader.youtube
-
-    upload_videos(uploader)
-
-    # add_to_playlist(youtube, "wdhWyUWEHv4")
-
-
-def upload_videos(uploader):
+def upload_videos(service):
     video_files = []
     for file in os.listdir(UPLOAD_DIRECTORY):
         video_files.append(file)
         video_files.sort()
 
     for file in video_files:
-        upload_response = upload_video(uploader, file)
-        time.sleep(30)
+        file_path = f"{UPLOAD_DIRECTORY}/{file}"
+        video_id = upload_video(service, file[:-4].replace("_", " "), file_path)
+
+        print(f"Deleting {file_path}...")
+        os.remove(file_path)
+
+        video_date = datetime.strptime(file[:-4], PHOTO_DATETIME_FORMAT)
+
+        playlist_id = get_playlist_id_from_date(service, video_date)
+
+        insert_video_into_playlist(service, video_id, playlist_id)
+        time.sleep(5)
 
 
-def add_to_playlist(youtube, video_id):
-    try:
-        channel = youtube.playlists().list(channelId="UCO8DUUQN0rVnvsjmrNiOjrg", part="snippet,contentDetails").execute()
-        # add_video_request=youtube.playlistItem().insert(
-        #     part="snippet",
-        #     body={
-        #         'snippet': {
-        #           'playlistId': playlistID,
-        #           'resourceId': {
-        #                   'kind': 'youtube#video',
-        #               'videoId': videoID
-        #             }
-        #         #'position': 0
-        #         }
-        #     }
-        # ).execute()
+def main():
+    service = get_authenticated_service()
 
-        print(channel)
-    except googleapiclient.errors.HttpError as e:
-        print (e)
+    upload_videos(service)
 
 
 if __name__ == "__main__":
-    main2()
+    main()
---
+++ youtube_uploader/utils/youtube.py
@@ -0,0 +1,157 @@
+import json
+import logging
+import os
+import os.path
+from datetime import datetime
+
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from googleapiclient.http import MediaFileUpload
+
+CLIENT_SECRET_FILE = "client_secret_252723081857-nr72rralklriqt6mh3d1d5idm1kbh8a9.apps.googleusercontent.com.json"
+
+# client_secret_252723081857-nr72rralklriqt6mh3d1d5idm1kbh8a9.apps.googleusercontent.com.json
+# client_secret_551581634693-mrsqtg54795liv2e5djvaph46c10fbvu.apps.googleusercontent.com.json
+
+YOUTUBE_UPLOAD_SCOPES = [
+    "https://www.googleapis.com/auth/youtube.upload",
+    "https://www.googleapis.com/auth/youtubepartner-channel-audit",
+    "https://www.googleapis.com/auth/youtubepartner",
+]
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
+MISSING_CLIENT_SECRETS_MESSAGE = ""
+
+PLAYLIST_MAX_RESULTS = 50
+
+logging.basicConfig()
+logging.getLogger().setLevel(logging.ERROR)
+
+
+def get_authenticated_service():
+    credentials = _get_user_credentials()
+    return build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, credentials=credentials)
+
+
+def _get_user_credentials():
+    if os.path.isfile("credentials.json"):
+        return Credentials.from_authorized_user_file("credentials.json")
+    flow = InstalledAppFlow.from_client_secrets_file(
+        CLIENT_SECRET_FILE, YOUTUBE_UPLOAD_SCOPES
+    )
+    credentials = flow.run_local_server()
+    with open("credentials.json", "w") as file:
+        json.dump(json.loads(credentials.to_json()), file)
+    return credentials
+
+
+def upload_video(service, name, file_path):
+    print("Uploading Video...")
+    try:
+        upload_response = (
+            service.videos()
+            .insert(
+                part="snippet,status",
+                body={
+                    "snippet": {"categoryId": "17", "description": "", "title": name},
+                    "status": {"privacyStatus": "public"},
+                },
+                media_body=MediaFileUpload(file_path),
+            )
+            .execute()
+        )
+    except HttpError as e:
+        logging.error(e.reason)
+        exit()
+
+    return upload_response["id"]
+
+
+def insert_video_into_playlist(service, video_id, playlist_id):
+    print(f"Inserting video {video_id} into playlist {playlist_id}")
+    try:
+        service.playlistItems().insert(
+            part="snippet",
+            body={
+                "snippet": {
+                    "playlistId": playlist_id,
+                    "resourceId": {"kind": "youtube#video", "videoId": video_id},
+                }
+            },
+        ).execute()
+    except HttpError as e:
+        logging.error(e.reason)
+        exit()
+
+
+def get_playlist_id_from_date(service, video_date: datetime):
+    playlist_name = video_date.strftime("%m/%d/%Y")
+    playlists = _get_list_of_playlists(service)
+
+    playlist_id_to_upload = [
+        playlist["id"]
+        for playlist in playlists
+        if playlist["snippet"]["title"] == playlist_name
+    ]
+    if playlist_id_to_upload:
+        print(f"Found existing playlist '{playlist_name}'")
+        return playlist_id_to_upload[0]
+
+    print("Creating playlist...")
+    try:
+        playlist_response = (
+            service.playlists()
+            .insert(
+                part="status,snippet",
+                body={
+                    "snippet": {"title": playlist_name},
+                    "status": {"privacyStatus": "public"},
+                },
+            )
+            .execute()
+        )
+    except HttpError as e:
+        logging.error(e.reason)
+        exit()
+
+    return playlist_response["id"]
+
+
+def _get_list_of_playlists(service):
+    channel_id = _get_users_youtube_channel_id(service)
+
+    playlists = []
+    playlists_response = (
+        service.playlists()
+        .list(channelId=channel_id, part="snippet", maxResults=PLAYLIST_MAX_RESULTS)
+        .execute()
+    )
+
+    playlists.extend(playlists_response["items"])
+
+    next_page_token = playlists_response.get("nextPageToken")
+
+    while next_page_token:
+        playlists_response = (
+            service.playlists()
+            .list(
+                channelId=channel_id,
+                part="snippet",
+                maxResults=PLAYLIST_MAX_RESULTS,
+                pageToken=next_page_token,
+            )
+            .execute()
+        )
+        playlists.extend(playlists_response["items"])
+        next_page_token = playlists_response.get("nextPageToken")
+
+    return playlists
+
+
+def _get_users_youtube_channel_id(service):
+    channels = service.channels().list(part="id", mine=True).execute()
+    if "items" in channels and len(channels["items"]) == 1:
+        return channels["items"][0]["id"]
+    logging.error("Could not get channels")
